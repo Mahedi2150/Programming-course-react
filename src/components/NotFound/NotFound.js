96027da3de14cbe033db2a27994f9c6a633c9c6d
Mahedi Hasan Bijoy <64260243+Mahedi2150@users.noreply.github.com>
2021-10-04 18:00:48 +0600

    about page added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import "./NotFound.css"
 const NotFound = () => {
     return (
-        <div>
-            <h2>404. Not found</h2>
+        <div >
+           <img className="image" src="https://drudesk.com/sites/default/files/2018-02/404-error-page-not-found.jpg" alt="" />
         </div>
     );
 };
